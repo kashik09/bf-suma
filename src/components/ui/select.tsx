c0f987f063ecad2878559d2f12f3e65c0eb22e43
kashik09 <kashiku789@gmail.com>
2026-03-13 11:08:06 +0300

    feat(ui): add base UI components (part 4/5)

---
+++ src/components/ui/select.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+export type SelectProps = React.SelectHTMLAttributes<HTMLSelectElement>;
+
+export const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
+  ({ className, children, ...props }, ref) => (
+    <select
+      ref={ref}
+      className={cn(
+        "h-10 w-full rounded-md border border-slate-300 bg-white px-3 text-sm outline-none ring-brand-500 focus:ring-2",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </select>
+  )
+);
+Select.displayName = "Select";
