6b7e84d49824263bedd25372fb897d30b7c455fd
kashik09 <kashiku789@gmail.com>
2026-03-29 21:59:52 +0300

    style(store): refresh browse page shells and loading spacing (part 2/2)

--- src/app/(store)/shop/loading.tsx
+++ src/app/(store)/shop/loading.tsx
@@ -3,7 +3,7 @@ import { LoadingState } from "@/components/ui/loading-state";
 
 export default function ShopLoading() {
   return (
-    <PageContainer className="py-10">
+    <PageContainer className="py-10 sm:py-12">
       <LoadingState label="Loading products..." />
     </PageContainer>
   );
