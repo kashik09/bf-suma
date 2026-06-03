967830d59a8d1400ad46d32a4a069442c70d6aee
kashik09 <kashiku789@gmail.com>
2026-06-03 22:25:13 +0300

    chore(deps): add recharts for dashboard charts
    Install recharts library for data visualization.
    
    Update next.config.ts with Sentry configuration.

--- next.config.ts
+++ next.config.ts
@@ -25,40 +25,19 @@ const nextConfig: NextConfig = {
   // Security headers are set in src/middleware.ts (single source of truth)
 };
 
-export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+// TEMPORARILY DISABLED SENTRY TO DEBUG 404 ISSUE
+export default nextConfig;
 
-  org: "kashi-kweyu",
-
-  project: "bf-suma",
-
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  tunnelRoute: "/monitoring",
-
-  webpack: {
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
-
-    // Tree-shaking options for reducing bundle size
-    treeshake: {
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
-      removeDebugLogging: true,
-    },
-  }
-});
+// export default withSentryConfig(nextConfig, {
+//   org: "kashi-kweyu",
+//   project: "bf-suma",
+//   silent: !process.env.CI,
+//   widenClientFileUpload: true,
+//   tunnelRoute: "/monitoring",
+//   webpack: {
+//     automaticVercelMonitors: true,
+//     treeshake: {
+//       removeDebugLogging: true,
+//     },
+//   }
+// });
